4a58ac0ea8d2d7a914e8f17ad4f0848a7b3c3aa6
rasmuseeg <rasmuseeg@outlook.com>
2020-06-16 19:20:44 +0200

    Sln changes

---
+++ Umbraco.ContentApps.Comments/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPoco;
+
+namespace Umbraco.ContentApps.Comments.Models
+{
+    public class Comment
+    {
+        
+    }
+}
